08a21c5f29f6ea1c65ca4cdef701c346caecacc0
Farjana-Fariha <farjanafariha507@gmail.com>
2024-02-15 16:58:05 +0600

    hightlight buttons

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-// show and hide page
-function play(){
-   hideElementById('home');
-   showElementById('playground')
-}
+
 function getRandomAlphabet(){
    const alphabet = generateAlphabet();
    // show Alphabet
    const showAlphabet = document.getElementById('show-alphabet');
    showAlphabet.innerText = alphabet;
    // Hightlight alphabet background
-   
+   hightlightBg(alphabet);
+
 
 }
-getRandomAlphabet()
+// show and hide page
+function play(){
+   hideElementById('home');
+   showElementById('playground');
+   getRandomAlphabet();
+}
+
--- js/utility.js
+++ js/utility.js
@@ -11,10 +11,16 @@ function showElementById(elementId){
 // Generate Alphabet
 function generateAlphabet(){
    const alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   const alphabetArray = alphabetString.split('');
+   const alphaLowerCase = alphabetString.toLowerCase();
+   const alphabetArray = alphaLowerCase.split('');
 
    // generate alphabet
    const index = Math.round(Math.random()*25);
    const alphabet = alphabetArray[index];
    return alphabet;
+}
+// Hightlight alphabet
+function hightlightBg(elementId){
+   const getAlphabet = document.getElementById(elementId);
+   getAlphabet.classList.add('bg-orange-300')
 }
